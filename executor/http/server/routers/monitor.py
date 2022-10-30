7aa223053afc6535a393182f42054a21cae2d05e
Nanguage <Nanguage@yahoo.com>
2022-10-30 21:54:40 +0800

    support view log in monitor mode

--- executor/http/server/routers/monitor.py
+++ executor/http/server/routers/monitor.py
@@ -1,3 +1,4 @@
+import typing as T
 from pathlib import Path
 from fastapi import APIRouter, HTTPException, status
 
@@ -8,7 +9,8 @@ from .. import config
 from ..utils import ser_job
 
 if config.monitor_cache_path is not None:
-    jobs = Jobs(Path(config.monitor_cache_path))
+    cache_path = Path(config.monitor_cache_path)
+    jobs = Jobs(cache_path / "jobs")
 else:
     raise ValueError("Monitor cache path is not provided, please set it in config.")
 
@@ -28,3 +30,33 @@ async def get_all_jobs_from_cache():
     for job in jobs.all_jobs():
         resp.append(ser_job(job))
     return resp
+
+
+def _read_then_return(job_id: str, log_file: str):
+    if config.monitor_cache_path is not None:
+        cache_path = Path(config.monitor_cache_path)
+    else:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST,
+            detail="Monitor cache path is not provided, please set it in config."
+        )
+    job_cache_dir = cache_path / job_id
+    try:
+        with open(job_cache_dir / log_file) as f:
+            content = f.read()
+        return {'content': content}
+    except Exception as e:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
+
+
+@router.get("/stdout/{job_id}")
+async def get_job_stdout(job_id: str):
+    return _read_then_return(job_id, "stdout.txt")
+
+
+@router.get("/stderr/{job_id}")
+async def get_job_stderr(job_id: str):
+    return _read_then_return(job_id, "stderr.txt")
--- tests/test_monitor_server.py
+++ tests/test_monitor_server.py
@@ -7,7 +7,7 @@ from executor.http.server import config
 from executor.engine import Engine, LocalJob, ThreadJob, ProcessJob
 
 
-def test_monitor_server():
+def test_list_all():
     engine = Engine()
 
     async def submit_job():
@@ -19,9 +19,30 @@ def test_monitor_server():
 
     asyncio.run(submit_job())
     config.monitor_mode = True
-    config.monitor_cache_path = engine.cache_dir / "jobs"
+    config.monitor_cache_path = engine.cache_dir
 
     app = create_app()
     client = TestClient(app)
     resp = client.get("/monitor/list_all")
     assert len(resp.json()) == 3
+
+
+def test_fetch_log():
+    def say_hello():
+        print("hello")
+
+    engine = Engine()
+    job = LocalJob(say_hello, redirect_out_err=True)
+
+    async def submit_job():
+        await engine.submit(job)
+        await engine.wait()
+
+    asyncio.run(submit_job())
+
+    config.monitor_cache_path = engine.cache_dir
+    app = create_app()
+    client = TestClient(app)
+    resp = client.get(f"/monitor/stdout/{job.id}")
+    assert resp.status_code == 200
+    assert resp.json()['content'] == "hello\n"
